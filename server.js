471f339338b5c6010c28d722c1d5f4f99b547673
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-23 15:55:17 +0000

    changed fetch request and added post

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
 console.log('Server-side code running');
 
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
 
-// start the express web server listening on 8080
-app.listen(8080, () => {
-  console.log('listening on 8080');
+// connect to the db and start the express server
+let db;
+
+// ***Replace the URL below with the URL for your database***
+const url =  'mongodb://clicks1:clicks1@ds245901.mlab.com:45901/clicks';
+// E.g. for option 2) above this will be:
+// const url =  'mongodb://localhost:21017/databaseName';
+
+MongoClient.connect(url, (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+  db = database;
+  // start the express web server listening on 8080
+  app.listen(8080, () => {
+    console.log('listening on 8080');
+  });
 });
 
 // serve the homepage
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/clicked', (req, res) => {
+  const click = {clickTime: new Date()};
+  console.log(click);
+  console.log(db);
+
+  db.collection('clicks').save(click, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('click added to db');
+    res.sendStatus(201);
+  });
 });
